8ddd3be6663cc480c6a0d441a146da8049b15ec8
Kim Sungsik <sskim0126@kookmin.ac.kr>
2022-03-27 19:34:09 +0900

    ItemCard component 추가 및 MainView dummy data 추가

--- src/mobile/View/MainView.js
+++ src/mobile/View/MainView.js
@@ -1,14 +1,45 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import Header from "../Component/Header";
 import Footer from "../Component/Footer";
+import ItemCard from "../Component/ItemCard";
 
 export default class MainView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          image: require("../assets/dummy_data/macbook.png"),
+          itemName: "맥북 프로 15인치",
+          location: "길음동",
+          reward: 150000,
+        },
+        {
+          image: require("../assets/dummy_data/airpods.png"),
+          itemName: "에어팟 프로",
+          location: "정릉동",
+          reward: 20000,
+        },
+        {
+          image: require("../assets/dummy_data/g102.png"),
+          itemName: "로지텍 G102 마우스",
+          location: "정릉동",
+          reward: 10000,
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Header />
-        <Text>MainView</Text>
+        <ScrollView>
+          {this.state.items.map((item, index) => {
+            return <ItemCard item={item} key={index} />;
+          })}
+        </ScrollView>
         <Footer />
       </View>
     );
