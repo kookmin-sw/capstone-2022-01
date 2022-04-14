44e82efd224aaa8c43e86cf4e6aeb5fa39dec2aa
Kim Sungsik <sskim0126@kookmin.ac.kr>
2022-04-14 23:57:41 +0900

    header에서 AlarmView로의 routing 추가

--- src/mobile/Component/Header.js
+++ src/mobile/Component/Header.js
@@ -1,55 +1,64 @@
-import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
+import React from 'react'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
   }
 
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         {this.props.isMain ? (
-            <TouchableOpacity
-              onPress={() =>
-                this.props.navigation.navigate("Location")
-              }
-            >
-              <Icon name="map-outline" size={30} />
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() =>
+              this.props.navigation.navigate('Location')
+            }
+          >
+            <Icon name="map-outline" size={30}/>
+          </TouchableOpacity>
         ) : (
           <TouchableOpacity
             onPress={() =>
               this.props.navigation.goBack()
             }
           >
-            <Icon name="arrow-back-outline" size={30} />
+            <Icon name="arrow-back-outline" size={30}/>
           </TouchableOpacity>
         )}
         <Text style={styles.logo}>O.LaF</Text>
-        <Icon name="notifications-outline" size={30} />
+        {
+          this.props.isMain ? (
+            <TouchableOpacity
+              onPress={() => {
+                this.props.navigation.navigate('Alarm')
+              }}>
+              <Icon name="notifications-outline" size={30}/>
+            </TouchableOpacity>
+          ) : <View/>
+        }
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: "12%",
-    paddingLeft: "3%",
-    paddingRight: "3%",
-    paddingBottom: "1%",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: '12%',
+    paddingLeft: '3%',
+    paddingRight: '3%',
+    paddingBottom: '1%',
     borderBottomWidth: 1,
-    borderBottomColor: "grey",
+    borderBottomColor: 'grey',
   },
   logo: {
-    alignSelf: "center",
+    alignSelf: 'center',
     fontSize: 35,
-    color: "#4080FF",
+    color: '#4080FF',
   },
-});
+})
